7cb58b273926de1728f9c24d938df544a2d6096d
Egor Selyanin <sel-egor@yandex.ru>
2020-09-04 13:07:45 +0300

    initial commit

---
+++ src/main/java/inc/zombie/dtmfweb/ServletInitializer.java
@@ -0,0 +1,13 @@
+package inc.zombie.dtmfweb;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DtmfWebApplication.class);
+	}
+
+}
---
+++ src/test/java/inc/zombie/dtmfweb/DtmfWebApplicationTests.java
@@ -0,0 +1,13 @@
+package inc.zombie.dtmfweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtmfWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
